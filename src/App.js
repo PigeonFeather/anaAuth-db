2fc9859105dc44d30d3df3ed582d70074ab3ffd1
Ana Piccio <may_piccio@yahoo.com>
2022-09-13 11:44:18 +1200

    put <Router> before <Switch> in App.js

--- src/App.js
+++ src/App.js
@@ -1,39 +1,41 @@
-import Container from "react-bootstrap/Container"; 
+import Container from "react-bootstrap/Container";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
-import { Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Account from "./Account";
 import FreeComponent from "./FreeComponent";
 import AuthComponent from "./AuthComponent";
 import ProtectedRoutes from "./ProtectedRoutes";
 
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <Container>
-      <Row>
-        <Col className="text-center">
-          <h1>React Authentication</h1>
-        
-        <section id="navigation">
-          <a href="/">Home</a>
-          <a href="/free">Free Component</a>
-          <a href="/auth">Auth Component</a>
-        </section>
-      </Col>  
-      </Row>
+    <Router>
+      <Container>
+        <Row>
+          <Col className="text-center">
+            <h1>React Authentication</h1>
 
-      { /* create routes here */}
-      <Switch>
+            <section id="navigation">
+              <a href="/">Home</a>
+              <a href="/free">Free Component</a>
+              <a href="/auth">Auth Component</a>
+            </section>
+          </Col>
+        </Row>
+
+        {/* create routes here */}
+        <Switch>
           <Route exact path="/" component={Account} />
           <Route exact path="/free" component={FreeComponent} />
           <ProtectedRoutes path="/auth" component={AuthComponent} />
-      </Switch>
-    </Container>
+        </Switch>
+      </Container>
+    </Router>
   );
 }
 
 export default App;
 
-// To start server: npm start 
+// To start server: npm start
